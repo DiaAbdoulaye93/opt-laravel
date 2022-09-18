e507c0e7c9787d1b1634264b9740b3259721ded2
Abdoulaye <diaabdoulayedjibril@gmail.com>
2022-09-18 15:41:33 +0000

    finalisation process

--- resources/views/vendor/template-otp/email.blade.php
+++ resources/views/vendor/template-otp/email.blade.php
@@ -5,14 +5,14 @@
 </head>
 <body>
 <p>
-    <div class="card">
-        <div class="card-header">
-            A2D Store
+    <div class="card" style="width:350px;border:2px solid #59ab6e">
+        <div class="card-header" style="width:350px;background-color:#59ab6e; height:50px;">
+          <span style="margin-left:38%;color:white"><span style="font-weight:bolder;font-size:32px;"> A</span>2D store</span> 
         </div>
-        <div class="card-body">
-            Bonjour,<br/>
-            Votre code de securité est: {{$otp}}<br/>
-            <span class="text-sm text-danger">PS: Ce code n'est valide qu'une seule fois</span>
+        <div class="card-body" style="width:350px; height:150px">
+            <h2 style="text-align: center;"> Bonjour !,</h2>
+            <span style="margin-left: 10%;">Votre code de securité est: {{$otp}}</span>
+            <h4 style="color:red;margin-left: 10%">PS: Ce code n'est valide qu'une seule fois</h4>
         </div>
     </div>
 
